3015f473194ee2edc11a116986007d22b391b85b
SantinoVitale <santino.pavese@gmail.com>
2022-08-06 17:26:59 -0300

    DOM y Eventos v0.2
    Se arregló el login
    Se está haciendo el agregar libro

--- BibliotecaPipo-Vitale/js/agregarLibro.js
+++ BibliotecaPipo-Vitale/js/agregarLibro.js
@@ -1,21 +1,42 @@
 class Libro{ // Creacion de la clase Libro
-    constructor(id, nombre, autor, editorial, isbn, fecha, lugar, estado) {
-        this.id = +(id)
-        this.nombre = nombre
+    constructor(id, titulo, autor, editorial, isbn, sinopsis, img, pasillo, estanteria, estado) {
+        this.id = id
+        this.titulo = titulo
         this.autor = autor
         this.editorial = editorial
         this.isbn = isbn
-        this.fecha = fecha
-        this.lugar = lugar
+        this.sinopsis = sinopsis
+        this.img = img
+        this.pasillo = pasillo
+        this.estanteria = estanteria
         this.estado = false
     }
 }
 
 const Libros = [
-    {id: "0", nombre: "El principito", autor: "Antoine de Saint-Exupéry", editorial: "Océano Historias gráficas", isbn: "978-84-7888-720-0", fecha: "30/07/2022", lugar: "Pasillo 7 - Estaneria 3"},
-    {id: "1", nombre: "Diez negritos", autor: "Agatha Christie", editorial: "Collins Crime Club+", isbn: "978-98-7831-765-6", fecha: "30/07/2022", lugar: "Pasillo 2 - Estaneria 4"}
+    {id: "0", titulo: "El principito", autor: "Antoine de Saint-Exupéry", editorial: "Océano Historias gráficas", isbn: "978-84-7888-720-0", sinopsis:"El principito es un libro", img:"./img1", pasillo: "Pasillo 7", estanteria:"Estaneria 3"},
+    {id: "1", titulo: "Diez negritos", autor: "Agatha Christie", editorial: "Collins Crime Club+", isbn: "978-98-7831-765-6", sinopsis:"Los diez negritos es un libro", img:"img2", pasillo: "Pasillo 2", estanteria:"Estaneria 4"}
 ] // Creación del Array y libros ya cargados
 
-Libros.push(new Libro("2", prompt("Ingrese nombre"), prompt("Ingrese autor"), prompt("Ingrese editorial"), prompt("Ingrese isbn"), prompt("Ingrese fecha de carga"), prompt("Ingrese lugar fisico del producto")))
+const button = document.querySelector(".form_button");
 
-console.log(Libros)
+let id = 2
+let titulo = document.getElementById("titulo").value
+let autor = document.getElementById("autor").value
+let editorial = document.getElementById("editorial").value
+let isbn = document.getElementById("isbn").value
+let sinopsis = document.getElementById("sinopsis").value
+let img = document.getElementById("img").value
+let pasillo = document.getElementById("pasillo").value
+let estanteria = document.getElementById("estanteria").value
+
+const agregarLibro = () => {
+    Libros.push(new Libro(id++, titulo, autor, editorial, isbn, sinopsis, img, pasillo, estanteria))
+    console.log(Libros)
+    alert("Espere un momento porfa")
+}
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    agregarLibro();
+});
--- BibliotecaPipo-Vitale/js/login.js
+++ BibliotecaPipo-Vitale/js/login.js
@@ -1,11 +1,15 @@
-function login(){
+const button = document.querySelector(".form_button");
+function login() {
     let usuario = document.getElementById("usuario").value;
     let password = document.getElementById("password").value;
-    let errorLogin = document.getElementById("error")
-
-    if(usuario == "Santino" && password == "Vitale"){
-        location.href  = "../pages/home.html";
+    let errorLogin = document.getElementById("error");
+    if (usuario == "Santino" && password == "Vitale") {
+        location.assign("./pages/home.html");
     } else {
         errorLogin.innerHTML = "<h6 style='color: red;'>ERROR! Usuario o contraseña ingresados incorrectamente</h6>"
     }
 }
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    login();
+});
