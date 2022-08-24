bfb3a74275e78d86eeef12af050b07c3207e9e4f
SantinoVitale <santino.pavese@gmail.com>
2022-08-24 10:08:20 -0300

    Librerias y Optimizando Código HotFix

--- BibliotecaPipo-Vitale/js/login.js
+++ BibliotecaPipo-Vitale/js/login.js
@@ -4,12 +4,7 @@ function login() {
     let usuario = document.getElementById("usuario").value;
     let password = document.getElementById("password").value;
     let errorLogin = document.getElementById("error");
-<<<<<<< HEAD
     usuario == "Santino" && password == "Vitale" ? location.assign("./pages/home.html") : swal("Biblioteca Pipo", "Usuario o contraseña incorrectos", "warning");
-=======
-    usuario == "Santino" && password == "Vitale" ? location.assign("./pages/home.html") : swal("Biblioteca Pipo", "El usuario o la contraseña son incorrectos", "warning");
->>>>>>> addc26ca32ddb52f83e60d8c7ed36f350ab4de69
-    
 }
 button.addEventListener("click", (e) => {
     e.preventDefault();
